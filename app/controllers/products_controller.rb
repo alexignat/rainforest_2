a9fd6d4d415fbbe2d30dbb9d46ec3120a883a7f6
alexignat <d.alexandru.ignat@gmail.com>
2015-06-30 15:56:49 -0400

    Added abilty to edit & update a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,25 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price_in_cents)
